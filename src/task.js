4131dfeb856682dfeae992ceb48caa64acb44c68
Ana Godines <ana2.1898@gmail.com>
2024-08-15 11:20:48 -0600

    termino de ui y task, inicio de index

--- src/task.js
+++ src/task.js
@@ -1,1 +1,32 @@
+//Traernos la lista de tareas del almacenamiento local
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+//Funcion para agregar una tarea
+export const addTask = (task) => {
+     const newTask = {
+        id: Date.now(),
+        text: task,
+        completed: false, 
+     };
+     task.push(newTask);
+     localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+//Funcion para eliminar una tarea
+export const deleteTask = (id) => {
+     tasks = tasks.filter((tasks) => tasks.id !== parseInt(id));
+     localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+//Funcion para actualizar la tarea
+export const taggleTask = (id) => {
+   tasks = tasks.map((task) => {
+    if(task.id === parseInt(id)){
+        task.completed = !task.completed;
+    }
+    return task;
+   });
+     localStorage.setItem("task", JSON.stringify(tasks));
+};
+//Funcion para llevar las tareas
+export const getTasks = () => tasks;
